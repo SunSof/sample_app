d246d08a67c314869d2b7d9436181c43d0d02cbd
SunSof <buketovasofi@gmail.com>
2022-08-18 17:47:56 +0400

    complited sign up and refactoring code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-  end 
+  end
 
   def new
     @user = User.new
@@ -9,15 +9,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save 
-      # Обработать успешное сохранение.
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      render 'new' 
+      render "new"
     end
   end
 
   private
+
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end 
+  end
 end
