9c4e7218db0bdf56ef0d2bca6d54d5e6ada7d95a
SunSof <buketovasofi@gmail.com>
2022-08-24 16:38:16 +0400

    completed logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,9 +8,15 @@ module SessionsHelper
   def current_user()
     @current_user ||= User.find_by(id: session[:user_id]) 
   end
+
   # Возвращает true, если пользователь зарегистрирован, иначе возвращает false. 
   def logged_in?()
     !current_user().nil? 
   end
-  
+
+  # Осуществляет выход текущего пользователя. 
+  def log_out()
+    session.delete(:user_id)
+    @current_user = nil 
+  end
 end
