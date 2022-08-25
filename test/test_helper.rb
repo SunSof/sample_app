4499314887b093a6a2e84dcb22b0e89c33fff234
SunSof <buketovasofi@gmail.com>
2022-08-25 16:29:24 +0400

    Finish log in/log out

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,21 @@ class ActiveSupport::TestCase
   def is_logged_in?
     !session[:user_id].nil? 
   end
+
+  # Выполняет вход тестового пользователя. 
+  def log_in_as(user, options = {})
+    password = options[:password] || 'password' 
+    remember_me = options[:remember_me] || '1'
+    if integration_test?
+      post login_path, params: { session: { email: user.email, password: password, remember_me: remember_me }}
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+# Возвращает true внутри интеграционного теста. 
+  def integration_test?()
+    defined?(post follow_redirect!) 
+  end
 end
