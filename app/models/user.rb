b3a9a1c4123beb4d56a22c17a5934a474dddf499
SunSof <buketovasofi@gmail.com>
2022-08-25 13:49:53 +0400

    create remember and forget session

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,8 +25,19 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64 
   end
 
-  def remember
+  def remember()
     self.remember_token = User.new_token
     update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Возвращает true, если указанный токен соответствует дайджесту. 
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token) 
+  end
+
+  # Забывает пользователя 
+  def forget()
+    update_attribute(:remember_digest, nil) 
   end
 end
