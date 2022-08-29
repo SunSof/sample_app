26760f394c30f2d0b4ed98609c65acd008dd526b
SunSof <buketovasofi@gmail.com>
2022-08-29 18:51:42 +0400

    implemented the output of all users

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   delete 'logout'=> 'sessions#destroy'
-  # patch 'edit_user_path' => 'users#edit'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
