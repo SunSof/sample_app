654822f7d7fa6001ba8680e3263cb289dae563e3
SunSof <buketovasofi@gmail.com>
2022-08-30 15:36:40 +0400

    create destroy users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
 
   def index()
     @users = User.paginate(page: params[:page])
@@ -29,7 +30,7 @@ class UsersController < ApplicationController
     end
   end
 
-  def update
+  def update()
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
@@ -38,6 +39,12 @@ class UsersController < ApplicationController
       flash[:danger] = "Incorrect email or password"
       render 'edit' 
     end
+  end
+  
+  def destroy() 
+    @user = User.find(params[:id]).destroy 
+    flash[:success] = "User deleted"  
+    redirect_to users_url
   end
 
   private
@@ -56,8 +63,13 @@ class UsersController < ApplicationController
     end
   end
   # Подтверждает права пользователя. 
-  def correct_user
+  def correct_user()
     @user = User.find(params[:id]) 
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Подтверждает наличие административных привилегий. 
+  def admin_user()
+    redirect_to(root_url) unless current_user.admin?
   end
 end
