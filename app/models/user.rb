2b06d66faee2342f0667b138871df5247a38e8ad
SunSof <buketovasofi@gmail.com>
2022-08-31 18:32:48 +0400

    AccountActivations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token 
+  before_save :downcase_email
+  before_create :create_activation_digest 
+  validates(:name, presence: true, length: { maximum: 50 })
 
   before_save { self.email = email.downcase }
   validates(:name, presence: true, length: { maximum: 50 })
@@ -39,5 +42,16 @@ class User < ApplicationRecord
   # Забывает пользователя 
   def forget()
     update_attribute(:remember_digest, nil) 
+  end
+
+  private
+# Преобразует адрес электронной почты в нижний регистр. 
+  def downcase_email
+    self.email = email.downcase 
+  end
+# Создает и присваивает токен активации и его дайджест. 
+  def create_activation_digest
+    self.activation_token = User.new_token 
+    self.activation_digest = User.digest(activation_token)
   end
 end
