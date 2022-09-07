7129f930a78cc39bd2e61d1b8c028ec93a768242
SunSof <buketovasofi@gmail.com>
2022-09-07 22:06:08 +0400

    Following to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token 
   has_many :microposts, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   before_save :downcase_email
   before_create :create_activation_digest 
   validates(:name, presence: true, length: { maximum: 50 })
@@ -73,6 +74,21 @@ class User < ApplicationRecord
   # Полная реализация приводится в разделе "Следование за пользователями". 
   def feed()
     Micropost.where("user_id = ?", id) 
+  end
+
+  # Выполняет подписку на сообщения пользователя. 
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)   
+  end
+
+  # Отменяет подписку на сообщения пользователя. 
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy 
+  end
+  
+  # Возвращает true, если текущий пользователь читает другого пользователя.
+  def following?(other_user)
+    following.include?(other_user) 
   end
 
   private
