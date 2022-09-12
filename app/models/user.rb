d77cb4961557628d9ee974dc6ab03376e7f09719
SunSof <buketovasofi@gmail.com>
2022-09-12 22:11:53 +0300

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,10 +72,12 @@ class User < ApplicationRecord
   def password_reset_expired?
     reset_sent_at < 2.hours.ago 
   end
-  # Определяет прото-ленту.
-  # Полная реализация приводится в разделе "Следование за пользователями". 
-  def feed()
-    Micropost.where("user_id = ?", id) 
+
+  # Возвращает ленту сообщений. 
+  def feed
+    following_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids})
+    OR user_id = :user_id", user_id: id)
   end
 
   # Выполняет подписку на сообщения пользователя. 
